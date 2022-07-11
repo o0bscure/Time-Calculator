a53b96642e8670649999cf014c14171534572882
M.Ghanem <86367386+o0bscure@users.noreply.github.com>
2022-07-11 19:37:53 +0300

    Update version 2.py

---
+++ version 2.py
@@ -0,0 +1,77 @@
+def add_time(start,duration):
+    
+    start = start.split(" ")
+    tod = str(start[1]).upper()
+    startlst = start[0].split(":")
+    start_hour = int(startlst[0])
+    start_min = int(startlst[1])
+    
+    
+    duration = duration.split(":")
+    duration_hours = int(duration[0])
+    duration_mins = int(duration[1])
+    
+
+    
+    day_count = 0
+    hr_count = 0
+    hrs = 0
+    mins = 0
+    min_count = 0
+    
+    if duration_mins == 0 :
+        mins = start_min
+    else:
+        mins = start_min
+        #the mins output should always start with the actual start minutes, then keep adding to it according to the duraton
+        for minute in range(duration_mins):
+            mins = mins + 1
+            if mins > 59 :
+                #if the minutes pass 60, subtract that number, then add an hour                    
+                hrs = hrs + 1
+                mins = mins - 60
+            print(mins)
+    if duration_hours == 0: 
+        hrs = start_hour + hrs
+    else:
+        for hour in range(duration_hours):
+            hr_count = hr_count + 1
+            if hrs == 0:
+                hrs = start_hour +1
+            else:
+                hrs = hrs + 1
+                if hrs == 12:
+                    if tod == "PM":       #if its 12 PM
+                        tod = "AM"        #make it 12 AM
+                        day = "Next day"  # move on to the next day 
+                    elif tod =="AM":      # if its 12 AM  
+                        tod = "PM"        #switch it to PM
+                    print(hrs,tod)
+                if hrs > 12:
+                    hrs = hrs - 12     # if hours is 13 or higher  , take 12 out of it
+            if hr_count == 24:
+                day_count = day_count + 1
+                hr_count = 0
+             
+    
+    #need to do somthine about the pm and am
+                
+    day = f"{day_count} days later"
+                
+    
+        
+        
+    if len(str(mins)) == 1 : mins = "0" + str(mins)
+    
+    
+    if day_count > 0:
+        return f"Time now is {hrs}:{mins} {tod} ({day})"
+    else:
+        return f"Time now is {hrs}:{mins} {tod}"
+        
+        
+    
+print(add_time("3:30 PM", "0:50"))
+
+times = [("3:00 PM", "3:10"),("11:30 AM", "2:32"),("11:43 AM", "00:20"),("10:10 PM", "3:30"),("11:43 PM", "24:20"),("6:30 PM", "205:12")]
+
