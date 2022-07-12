7af1dcc75d3d5d233f6f2cf25a42ef3ed3f6f42d
M.Ghanem <86367386+o0bscure@users.noreply.github.com>
2022-07-12 21:20:13 +0300

    Update version 2.py

--- version 2.py
+++ version 2.py
@@ -37,7 +37,7 @@ def add_time(start,duration):
         if hrs == 12:
                 if tod == "PM":       #if its 12 PM
                     tod = "AM"        #make it 12 AM
-                    day = "Next day"  # move on to the next day 
+                    day = "next day"  # move on to the next day 
                 elif tod =="AM":      # if its 12 AM  
                     tod = "PM"        #switch it to PM
     else:
@@ -45,11 +45,11 @@ def add_time(start,duration):
         #the hours here is 12, the start hours is 11, then only added an extra hours vuz of the minutes.
         for hour in range(duration_hours):
             if hrs == 12:
-                if tod == "PM":       #if its 12 PM
-                    tod = "AM"        #make it 12 AM
-                    day = "Next day"  # move on to the next day 
-                elif tod =="AM":      # if its 12 AM  
-                    tod = "PM"        #switch it to PM
+                if tod == "PM":       
+                    tod = "AM"        
+                    day = "next day"  
+                elif tod =="AM":     
+                    tod = "PM"      
             hr_count = hr_count + 1
             hrs = hrs + 1
             #need to move the tod manipulation outside of the hours loop (why)
@@ -57,23 +57,24 @@ def add_time(start,duration):
                 hrs = hrs - 12     # if hours is 13 or higher  , take 12 out of it
             if hr_count == 24:
                 day_count = day_count + 1
-                hr_count = 0           
+                hr_count = 0     
       
     if len(str(mins)) == 1 : mins = "0" + str(mins)      
     
     if day_count > 1:
         day = f"{day_count} days later"     #this applies if the hour count is > 24
-        #in some cases the hour count isnt 24, meaning the day count still 0, but u still wanna move on to the next day, cuz the starting hour is 11 PM and u added for example 2 hours, meaning its the next day             
-    
-    if day_count > 0:
+        #in some cases the hour count isnt 24, meaning the day count still 0, but u still wanna move on to the next day, cuz the starting hour is 11 PM and u added for example 2 hours, meaning its the next day      
+               
+    if day == "next day":
+        return f"Time now is {hrs}:{mins} {tod} ({day})"     
+    elif day_count > 0:
         return f"Time now is {hrs}:{mins} {tod} ({day})"
     else:
         return f"Time now is {hrs}:{mins} {tod}"
     
-    #need to print the (next day) case!!
         
     
-print(add_time("10:10 PM", "3:30"))
+print(add_time("11:43 PM", "24:20"))
 
-times = [("3:00 PM", "3:10"),("11:30 AM", "2:32"),("11:43 AM", "00:20"),("10:10 PM", "3:30"),("11:43 PM", "24:20"),("6:30 PM", "205:12")]
+times = [("3:00 PM", "3:10"),("11:30 AM", "2:32"),("11:43 AM", "00:20"),("10:10 PM", "3:30"),("11:43 PM", "24:20"),("6:30 PM", "205:12"),("8:16 PM", "466:02"),("11:59 PM","24:05")]
 
